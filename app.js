8de5c5baabf886280d3882dab90ac7d429025df2
Muhammad Ziauddin Brohi <155188806+ziauddinbrohi@users.noreply.github.com>
2024-09-01 01:42:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,137 @@
+// Assignment 02
+//Chapter 03
+
+//Question 01
+// var age =16;
+// alert("my age is",age);
+
+// // Question 02
+// var visited = "14 Times";
+// alert("you have visited website", visited );
+
+// Question 03
+// var birthyear = 2008
+// document.write("<h1>My birth year is</h1>"+ birthyear)
+// document.write("<h1>Data type of my declared variable is</h1>"+ typeof birthyear)
+
+// Question 04
+// var visitorName = "Ziauddin brohi";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+// //Chapter 04
+// Question 01
+// var fullname="Muhammad Ziauddin brohi"
+// var Birthplace="karachi"
+// var birthyear=2008
+
+//Question 02
+//Legal Variables
+// var fullname$="Muhammad Ziauddin brohi"
+// var Birth_place="karachi"
+// var birthyear2008=2008
+// var user="window"
+// var full$Name_1="Zia"
+
+//Illeagle variables
+// var full-name="Muhammad Ziauddin brohi"
+// var Birth place="karachi"
+// var birth/year2008=2008
+// var #user="window"
+// var full$|Name_1="Zia"
+
+//Question 03
+//  document.write("<h1>Rules for naming JS variables</h1>" + "<br>Variable names can only contain number, $ , and _.For example $my_1stVariable"+"<br>Variables must begin with a Letter, $ or _.For example $name, _name or name"+"<br>Variable names are case Sesitive" +"<br>Variable names should not be JS Keywords")
+
+// Chapter 05
+// // Question 01
+// var num1=3
+// var num2=5
+// var ans1= num1 + num2
+// document.write("Sum of "+num1+" and "+num2+ " is " +ans1 )
+
+// // Question 02
+// Subtraction
+// var ans2= num1 - num2
+// document.write("Subtraction of "+num1+" and "+num2+ " is " +ans2 )
+
+//Multiplication
+// var ans3= num1 * num2
+// document.write("Multiplication of "+num1+" and "+num2+ " is " +ans3 )
+
+// Division
+// var ans4= num1 / num2
+// document.write("Division of "+num1+" and "+num2+ " is " +ans4 )
+
+//Modulus
+// var ans5= num1 % num2
+// document.write("Subtraction of "+num1+" and "+num2+ " is " +ans5)
+
+//Question 03
+// var laptopPrice;
+// document.write("Value after variable declaration is:" + laptopPrice+"<br>")
+
+//  laptopPrice=5
+// document.write("Initial Value:" + laptopPrice+ "<br>")
+
+// laptopPrice++
+// document.write("Initial Value:" + laptopPrice+"<br>")
+
+// laptopPrice+= 7
+// document.write("Value after addision is"+ laptopPrice+"<br>")
+
+// laptopPrice--;
+// document.write("Value after decrement is: " + laptopPrice + "<br>");
+// let remainder = laptopPrice % 3;
+// document.write("The remainder is: " + remainder);
+
+//Question 04 
+
+//  var ticketPrice = 600;
+//  var numberOfTickets = 5;
+//  var totalCost = ticketPrice * numberOfTickets;
+//  document.write("The cost of buying " + numberOfTickets + " tickets is: " + totalCost + " PKR.");
+
+//Question 05
+// let number = 4;
+// document.write("<h2>Multiplication Table of " + number + "</h2>");
+// document.write(number + " x 1 = " + (number * 1) + "<br>");
+// document.write(number + " x 2 = " + (number * 2) + "<br>");
+// document.write(number + " x 3 = " + (number * 3) + "<br>");
+// document.write(number + " x 4 = " + (number * 4) + "<br>");
+// document.write(number + " x 5 = " + (number * 5) + "<br>");
+// document.write(number + " x 6 = " + (number * 6) + "<br>");
+// document.write(number + " x 7 = " + (number * 7) + "<br>");
+// document.write(number + " x 8 = " + (number * 8) + "<br>");
+// document.write(number + " x 9 = " + (number * 9) + "<br>");
+// document.write(number + " x 10 = " + (number * 10) + "<br>");
+
+//Question 06 
+// let celsiusTemp = 30;
+
+// let fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+// document.write(celsiusTemp + "°C is " + fahrenheitTemp + "°F<br>");
+
+// fahrenheitTemp = 86; 
+
+// celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+// document.write(fahrenheitTemp + "°F is " + celsiusTemp.toFixed(2) + "°C")
+
+// Question 04
+// var item01 = 650 
+// var quanItem01 = 3
+// var item02 = 100
+// var quanItem02 = 7
+// var shipcharg = 100
+// var totalCost = (quanItem01*item01)+(quanItem02*item02)+(shipcharg)
+
+// document.write("Price of item 1 is"+item01 +"<br>")
+// document.write("Quantity of item 1 is"+quanItem01+"<br>")
+// document.write("Price of item 2 is"+item02 +"<br>")
+// document.write("Quantity of item 2 is"+quanItem02+"<br>")
+// document.write("Shipping Charges"+shipcharg+"<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Total cost of your order is "+totalcost)
+
